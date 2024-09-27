9351e4aae142f96a9aacb8e27a5c965175e63bbe
Troels Smidt <tsm@edora.dk>
2024-09-27 15:00:13 +0200

    chapter 12 wip

---
+++ app/lib/actions.ts
@@ -0,0 +1,12 @@
+﻿'use server';
+
+export async function createInvoice(formData: FormData) {
+    //const rawFormData = Object.fromEntries(formData.entries()); for more fields instead of just 3
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    };
+    // Test it out:
+    console.log(rawFormData);
+}
