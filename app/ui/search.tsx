b707c26b14fc16c53001ab91aabc7d05d88dd58e
Troels Smidt <tsm@edora.dk>
2024-10-02 14:49:07 +0200

    last chapter done

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -21,7 +21,7 @@ export default function Search() {
                 params.delete('query');
             }
             replace(`${pathname}?${params.toString()}`);
-    }, 300);
+    });
     
 
     return (
